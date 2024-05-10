6c86425b230e94d95c9f2a692250c7b9fdb7a516
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-05-10 23:56:27 +0530

    Updated script.js

--- script/script.js
+++ script/script.js
@@ -1,9 +1,6 @@
     $(document).ready(function(){
+
     $('#dashboard-section').css({display: 'block'});
-    $('#items-section').css({display: 'none'});
-    $('#order-section').css({display: 'none'});
-    $('#customer-section').css({display: 'none'});
-    $('#staff-section').css({display: 'none'});
 
     $('#nav-dashboard').on('click', () => {
     $('#dashboard-section').css({display: 'block'});
