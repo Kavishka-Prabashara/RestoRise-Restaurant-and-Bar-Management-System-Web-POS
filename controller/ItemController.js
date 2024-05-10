2703558561872a760ab4d36214ee3d7a326f7130
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-05-10 21:29:34 +0530

    Updated ItemController.js submit button get value done

---
+++ controller/ItemController.js
@@ -0,0 +1,12 @@
+$("#student-submit").on('click', () => {
+
+    var supplierId = $('#supplierId').val();
+    var supplierName = $('#supplierName').val();
+    var suppliedItemCategory = $('#dropdownItemCategory').val();
+    var itemId = $('#itemId').val();
+    var itemName = $('#itemName').val();
+    var itemQTY = $('#itemQTY').val();
+    var unitPrice = $('#itemUnitPrice').val();
+    var suppliedDate = $('#suppliedDate').get().val();
+
+});
