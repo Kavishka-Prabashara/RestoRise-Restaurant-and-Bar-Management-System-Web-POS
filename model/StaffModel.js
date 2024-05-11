32f206505b4d7c454f736cfe2126d1a135bd8b60
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-05-11 14:52:30 +0530

    Updated StaffModel.js class

---
+++ model/StaffModel.js
@@ -0,0 +1,25 @@
+export default class CustomerModel {
+    constructor(staffID , staffName , staffNIC , staffDob , staffContactNo) {
+        this._staffID = staffID ;
+        this._staffName = staffName ;
+        this._staffNIC = staffNIC ;
+        this._staffDob = staffDob ;
+        this._staffContactNo = staffContactNo ;
+    }
+
+    get staffID() {
+        return this._staffID ;
+    }
+    get staffName () {
+        return this._staffName ;
+    }
+    get staffNIC () {
+        return this._staffNIC ;
+    }
+    get staffDob () {
+        return this._staffDob ;
+    }
+    get staffContactNo () {
+        return this._staffContactNo ;
+    }
+}
