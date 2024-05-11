be9516ac315a7bcd1b2c73f65bff9ab4ee93fbfa
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-05-11 14:29:13 +0530

    CustomerModel.js class is Updated

--- model/CustomerModel.js
+++ model/CustomerModel.js
@@ -1,13 +1,13 @@
 export default class CustomerModel {
     constructor(customerID, customerName, customerNIC, customerContactNo) {
-        this._customerID = customerId;
+        this._customerID = customerID;
         this._customerName = customerName ;
         this._customerNIC = customerNIC ;
         this._customerContactNo = customerContactNo ;
     }
 
     get customerID  (){
-        return this._customerId;
+        return this._customerID;
     }
     get customerName() {
         return this._customerName ;
@@ -19,7 +19,7 @@ export default class CustomerModel {
         return this._customerContactNo ;
     }
     set customerID (customerID) {
-        this._customerId = customerID ;
+        this._customerID = customerID ;
     }
     set customerName (customerName) {
         this._customerName = customerName ;
