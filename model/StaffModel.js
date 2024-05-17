51c27fe43bfbb65c42efb90f3af5ee4e68a9fee8
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-05-17 22:30:32 +0530

    Updated StaffModel.js

--- model/StaffModel.js
+++ model/StaffModel.js
@@ -1,9 +1,10 @@
 export default class StaffModel {
-    constructor(staffID , staffName , staffNIC , staffDob , staffContactNo) {
+    constructor(staffID , staffName , staffNIC , staffDob , jobCategory ,staffContactNo) {
         this._staffID = staffID ;
         this._staffName = staffName ;
         this._staffNIC = staffNIC ;
         this._staffDob = staffDob ;
+        this._jobCategory = jobCategory ;
         this._staffContactNo = staffContactNo ;
     }
 
@@ -19,7 +20,28 @@ export default class StaffModel {
     get staffDob () {
         return this._staffDob ;
     }
+    get jobCategory () {
+        return this._jobCategory ;
+    }
     get staffContactNo () {
         return this._staffContactNo ;
+    }
+    set staffID(staffID) {
+        this._staffID = staffID;
+    }
+    set staffName (staffName) {
+        this._staffName = staffName;
+    }
+    set staffNIC (staffNIC) {
+        this._staffNIC = staffNIC;
+    }
+    set staffDob (staffDob) {
+        this._staffDob = staffDob;
+    }
+    set jobCategory (jobCategory) {
+        this._jobCategory = jobCategory;
+    }
+    set staffContactNo (staffContactNo) {
+        this._staffContactNo = staffContactNo;
     }
 }
