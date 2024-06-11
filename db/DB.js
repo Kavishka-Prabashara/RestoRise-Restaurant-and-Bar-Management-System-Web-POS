22ba4b99616d0bbb980724384c30dca73b53cfcd
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-06-11 22:47:40 +0530

    Updated DB.js

--- db/DB.js
+++ db/DB.js
@@ -1,5 +1,21 @@
- export var users = [];
- export var items = [];
- export var customers = [];
- export var staffs = [];
-  export var orders = [];
+export var users = [
+ { email: 'kavishkaprabashara@gmail.com', name: 'Kavishka', address: 'Induruwa', mobileNo: '0777289976', password: '1234', confirmPassword: '1234' }
+];
+
+export var items = [
+ { supplierId: 'SP001', supplierName: 'Amal', itemCategory: 'Kitchen', itemId: 'I001', itemName: 'Corn Powder', itemQTY: 100, itemUnitPrice: 100, suppliedDate: '2024-01-01' },
+ { supplierId: 'SP002', supplierName: 'Nimal', itemCategory: 'Bartender', itemId: 'Beer', itemName: 'Wild Apple', itemQTY: 200, itemUnitPrice: 850, suppliedDate: '2024-02-01' }
+];
+
+export var customers = [
+ { customerID: 'C001', customerName: 'Kavishka', customerNIC: '200109003225', customerContactNo: '0777289976' },
+ { customerID: 'C002', customerName: 'Prabashara', customerNIC: '200109003226', customerContactNo: '0715997463' }
+];
+
+export var staffs = [
+{ staffId: 'S001' , staffName : 'Shanilka' , staffNIC:'200209001234' , staffDob: '2002-01-01', jobCategory: 'Kitchen' , staffContactNo: '0771234567' },
+{ staffId: 'S002' , staffName : 'Dulmin' , staffNIC:'200109002235' , staffDob: '2001-01-01', jobCategory: 'Bartender' , staffContactNo: '0771234568' },
+{ staffId: 'S003' , staffName : 'Omesh' , staffNIC:'200209002236' , staffDob: '2002-01-02', jobCategory: 'Bartender' , staffContactNo: '0771234560' }];
+
+
+export var orders = [];
