4df28de227d501701747304580ee3ba6455dca5c
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-06-14 06:17:11 +0530

    Created OrderDetailsModel.js

---
+++ model/OrderDetailsModel.js
@@ -0,0 +1,9 @@
+export class orderModel{
+    constructor(order_id,item_id,itemUnitPrice,itemQTY) {
+        this.order_id=order_id;
+        this.item_id=item_id;
+        this.itemUnitPrice=itemUnitPrice;
+        this.itemQTY=itemQTY;
+    }
+
+}
