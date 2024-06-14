2ea810cf67bb40865ae1f734c5b4293fd610ddab
Kavishka Prabashara Wijerathna <kavishkaprabashara@gmail.com>
2024-06-14 06:17:41 +0530

    Updated script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-/*
+
     $(document).ready(function(){
 
         $('#login-section').css({display: 'block'});
@@ -21,7 +21,7 @@
             $('#staff-section').css({display: 'none'});
         });
 
-/!*        $('#loginSection-btnLogin').on('click', () => {
+        $('#loginSection-btnLogin').on('click', () => {
 
             $('#login-Section').css({display: 'none'});
             $('#signUp-section').css({display: 'none'});
@@ -31,7 +31,7 @@
             $('#order-section').css({display: 'none'});
             $('#customer-section').css({display: 'none'});
             $('#staff-section').css({display: 'none'});
-        });*!/
+        });
 
     $('#nav-dashboard').on('click', () => {
         $('#login-section').css({display: 'none'});
@@ -106,4 +106,151 @@
 
 });
 
-*/
+/*+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|+|*/
+
+    $(document).ready(function () {
+
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'none';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'none';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'none';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'none';
+
+
+        var counter = 0;
+        var c = 0;
+        var i = setInterval(function () {
+            $(".loading-page>.counter>h1").html(c + "%");
+            $(".loading-page>.counter>hr").css("width", c + "%");
+            counter++;
+            c++;
+
+            if (counter == 102) {
+                clearInterval(i);
+                const loadingScreen = document.querySelector('.loading-page');
+                loadingScreen.style.display = 'none';
+                myFunction();
+
+            }
+        }, 1);
+    });
+
+
+    const myFunction = function () {
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'block';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'block';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'none';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'none';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'none';
+
+    }
+
+    const myFunction1 = function () {
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'block';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'block';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'none';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'none';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'none';
+
+    }
+
+    const myFunction2 = function () {
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'block';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'block';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'none';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'none';
+
+    }
+
+    const myFunction3 = function () {
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'block';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'none';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'block';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'none';
+
+    }
+
+    const myFunction4 = function () {
+        const loadingScreen = document.querySelector('.home');
+        loadingScreen.style.display = 'none';
+
+        const loadingScreen3 = document.querySelector('#navBar');
+        loadingScreen3.style.display = 'block';
+
+        const loadingScreen2 = document.querySelector('.customer');
+        loadingScreen2.style.display = 'none';
+
+        const loadingScreen4 = document.querySelector('.item');
+        loadingScreen4.style.display = 'none';
+
+        const loadingScreen5 = document.querySelector('.order');
+        loadingScreen5.style.display = 'none';
+
+        const loadingScreen6 = document.querySelector('.order_Details');
+        loadingScreen6.style.display = 'block';
+
+    }
+
+
